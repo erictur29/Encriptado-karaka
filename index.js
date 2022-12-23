555b257e57ecbd4ab16db4bf9d7140de4af65f86
erictur29 <112108951+erictur29@users.noreply.github.com>
2022-12-23 17:46:34 +0100

    2 files modified

--- index.js
+++ index.js
@@ -3,16 +3,24 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
+const texto = document.querySelector('texto');
 
-let palabra=prompt("escribe algo");
-let palabraE
 
-palabra=palabra.replaceAll("a",'1')
+let palabra=texto;
+
+
+console.log(palabra);
+
+
+
+//let palabraE
+
+/*palabra=palabra.replaceAll("a",'1')
 palabra=palabra.replaceAll("e",'2')
 palabra=palabra.replaceAll("i",'3')
 palabra=palabra.replaceAll("o",'4')
-palabra=palabra.replaceAll("u",'5')
+palabra=palabra.replaceAll("u",'5')*/
 
 
 
-appDiv.innerHTML = `<h1>${palabra}</h1>`;
+//appDiv.innerHTML = `<h1>${palabra.textContent}</h1>`;
