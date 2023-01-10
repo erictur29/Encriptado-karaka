3e3dc5c0b8401ec1f1fd5374833261c9ac9876c4
erictur29 <112108951+erictur29@users.noreply.github.com>
2023-01-10 06:40:13 +0100

    2 files modified

--- index.js
+++ index.js
@@ -3,10 +3,21 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-const texto = document.querySelector('texto');
+const texto = document.querySelector('#texto');
+const encriptar = document.querySelector('button');
+
+encriptar.addEventListener('click', () => {
+  let palabra = texto.value;
+  palabra=palabra.replaceAll("a",'1');
+  palabra=palabra.replaceAll("e",'2');
+  palabra=palabra.replaceAll("i",'3');
+  palabra=palabra.replaceAll("o",'4');
+  palabra=palabra.replaceAll("u",'5');
+  return palabra;
+});
 
 
-let palabra=texto;
+/*let palabra=texto;
 
 
 console.log(palabra);
